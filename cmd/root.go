280cc3284bda7fad52b126e5a79198aa5bac76f2
rassakhatsky <dmitry@rassakhatsky.com>
2018-11-18 18:41:59 -0500

    Add command for auth service will print out results for now

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// flags are here
+var (
+	timeout int
+)
+
 var rootCmd = &cobra.Command{
 	// TODO: add desc
 	Use:   "tdgo",
 	Short: "tdgo",
 	Long:  `tdgo`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hey")
+		fmt.Println("Please use help to check all available commands:")
+		fmt.Println("> tdgo help")
 	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().IntVarP(&timeout, "timeout", "t", 90, "Default timeout")
+
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(authCmd)
 }
 
 func Execute() {
--- cmd/version.go
+++ cmd/version.go
@@ -6,10 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(versionCmd)
-}
-
 var versionCmd = &cobra.Command{
 	Use: "version",
 
